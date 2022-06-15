a459cf18e5e6741c46fd74843acac5b2876c5c49
Stephanie Ezinne <stephanieegbuonu@gmail.com>
2022-06-15 12:54:40 +0100

    She Code Africa technical assessment
    Javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 // complete the function
 function prime (num) {
   // code goes here
+  let isPrime;
+
+  if (num <= 1) { // one and numbers less than one are not prime numbers
+    return false;
+  }
+  if (num == 2) { // prime numbers begin from 2
+    return true; 
+  } 
+  
+  for(let i = 2; i < num; i++){
+    if (num % i === 0) { // checks if i divides with no remainder
+        isPrime = false;
+        break;  //break after the first true instance 
+      }
+      else {
+        isPrime = true;
+    }
+  } 
+  return isPrime
 }
+
 
 function solution (arg) {
   return prime(arg);
